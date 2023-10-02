f0ddc8f8ba0fe151f816d0b6fef8f3f34d5efd29
Gavrilov Andrey V <an.gavrilov@tinkoff.ru>
2023-10-02 14:26:16 +0300

    delete AccessController.doPrivileged

--- src/main/java/org/example/extentions/UIExtention.java
+++ src/main/java/org/example/extentions/UIExtention.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openqa.selenium.WebDriver;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UIExtention implements BeforeEachCallback, AfterEachCallback {
   private WebDriver driver;
+  private static final Logger LOGGER = LoggerFactory.getLogger(UIExtention.class);
 
   private List<Field> getAnnotatedFields(Class<? extends Annotation> annotaion, ExtensionContext extensionContext) {
     List<Field> set = new ArrayList<>();
@@ -39,17 +39,12 @@ public class UIExtention implements BeforeEachCallback, AfterEachCallback {
 
     for (Field field : fields) {
       if (field.getType().getName().equals(WebDriver.class.getName())) {
-        AccessController.doPrivileged((PrivilegedAction<Void>)
-            () -> {
-              try {
-                field.setAccessible(true);
-                field.set(extensionContext.getTestInstance().get(), driver);
-              } catch (IllegalAccessException e) {
-                throw new Error(String.format("Could not access or set webdriver in field: %s - is this field public?", field.getName()));
-              }
-              return null;
-            }
-        );
+        try {
+          field.setAccessible(true);
+          field.set(extensionContext.getTestInstance().get(), driver);
+        } catch (IllegalAccessException e) {
+          throw new Error(String.format("Could not access or set webdriver in field: %s - is this field public?", field.getName()));
+        }
       }
     }
   }
@@ -59,6 +54,7 @@ public class UIExtention implements BeforeEachCallback, AfterEachCallback {
   public void afterEach(ExtensionContext extensionContext) throws Exception {
     if (driver != null) {
       driver.close();
+      LOGGER.info("Driver closed");
       driver.quit();
     }
   }
