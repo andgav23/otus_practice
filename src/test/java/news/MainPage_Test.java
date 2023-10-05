b4b9646810678392c981d5734df39f629ebadd39
Gavrilov Andrey V <an.gavrilov@tinkoff.ru>
2023-10-05 19:32:20 +0300

    first working option

--- src/test/java/news/MainPage_Test.java
+++ src/test/java/news/MainPage_Test.java
@@ -5,19 +5,15 @@ import org.example.components.CoursesComponent;
 import org.example.data.CoursesData;
 import org.example.extentions.UIExtention;
 import org.example.pages.MainPage;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-import java.time.LocalDate;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(UIExtention.class)
@@ -28,20 +24,26 @@ public class MainPage_Test {
 
 
   @Test
-
-  public void mainPageOpened() {
+  public void courseFound() {
     new MainPage(driver).open();
-//    List<String> courseTitles = new CoursesComponent(driver).getCourseTitles();
-//    assertThat(courseTitles).contains(CoursesData.SYSTEM_ANALISYS.getName());
+    List<String> courseTitles = new CoursesComponent(driver).getCourseTitles();
+    assertThat(courseTitles).contains(CoursesData.SYSTEM_ANALISYS.getName());
+
+  }
+
+  @Test
+  public void latestCourseOpened () {
+    new MainPage(driver).open();
     CoursesComponent coursesComponent = new CoursesComponent(driver);
-    Map<WebElement, LocalDate> components = coursesComponent.getCoursesMap();
+    WebElement latestCourse = coursesComponent.getMaxStartDateCourse(coursesComponent.getCoursesMap());
     Actions actions = new Actions(driver);
 
-    actions.moveToElement(coursesComponent.getMaxStartDateCourse(components))
-        .perform();
+    actions
+        .moveToElement(latestCourse)
+        .pause(5000)
+        .build().perform();
 
-
-    //coursesComponent.clickCoursesItem(coursesComponent.getMaxStartDateCourse(components));
+    coursesComponent.clickCoursesItem(latestCourse);
   }
 
 
