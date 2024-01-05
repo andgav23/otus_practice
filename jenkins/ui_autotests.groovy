9e2bd409e5a2eeafed96a82af7d8fc0b1b853854
andgavuser <andgav23@rambler.ru>
2024-01-05 19:14:21 +0300

    minor

--- jenkins/ui_autotests.groovy
+++ jenkins/ui_autotests.groovy
@@ -21,7 +21,7 @@ node('maven_otus') {
             }
         }
                 stage("tests stage") {
-                    final foundFiles = sh(script: 'ls /home/jenkins', returnStdout: true).split()
+                    final foundFiles = sh(script: 'ls /home/jenkins/agent', returnStdout: true).split()
                     sh "echo 'Hello ${foundFiles}'"
 //                    sh "echo ${foundFiles}"
 
