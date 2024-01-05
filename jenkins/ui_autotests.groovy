3fddcfc26138b5a89f5fb32b5411b63076c38476
andgavuser <andgav23@rambler.ru>
2024-01-05 13:42:39 +0300

    minor

--- jenkins/ui_autotests.groovy
+++ jenkins/ui_autotests.groovy
@@ -13,27 +13,30 @@ node('maven_otus') {
                 env.setProperty(entry.key, entry.value)
             }
         }
+                stage("tests stage") {
+                    sh "mvn test -Dbrowser=${env.BROWSER_NAME}"
+        }
 //        stage("Checkout") {
 //            scm checkout
 //        }
-        stage("Running UI tests") {
-            if (env.BROWSER_NAME == "") {
-                echo "BROWSER_NAME parameter is required"
-                exit(1)
-            }
-            // sh "mvn test -Dbrowser=${env.BROWSER_NAME}"
-            status = sh(script: "mvn test -Dbrowser=${env.BROWSER_NAME}",
-                    returnStatus: true)
-            if (status == 1) {
-                currentBuild.status = "UNSTABLE"
-            }
-        }
-        stage("Allure report") {
-            allure(
-                    results: ['$WORKSPACE/target/allure-results'],
-                    disabled: false,
-                    reportBuildPolicy: 'ALWAYS'
-            )
-        }
+//        stage("Running UI tests") {
+//            if (env.BROWSER_NAME == "") {
+//                echo "BROWSER_NAME parameter is required"
+//                exit(1)
+//            }
+//            // sh "mvn test -Dbrowser=${env.BROWSER_NAME}"
+//            status = sh(script: "mvn test -Dbrowser=${env.BROWSER_NAME}",
+//                    returnStatus: true)
+//            if (status == 1) {
+//                currentBuild.status = "UNSTABLE"
+//            }
+//        }
+//        stage("Allure report") {
+//            allure(
+//                    results: ['$WORKSPACE/target/allure-results'],
+//                    disabled: false,
+//                    reportBuildPolicy: 'ALWAYS'
+//            )
+//        }
     }
 }
