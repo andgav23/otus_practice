1b6686d656373fe96ae85371e1a65c211a02fd1f
andgavuser <andgav23@rambler.ru>
2024-01-07 13:02:26 +0300

    initial

--- jenkins/runner.groovy
+++ jenkins/runner.groovy
@@ -2,11 +2,11 @@ pipeline {
     agent any
     stages {
         stage('build job-a') {
-            steps {
+
 //                build job: 'job-a', parameters: [[$class: 'StringParameterValue', name: 'BRANCH_ONE', value: "two"]]
                 for (branch in env.BRANCHES) {
                     echo "Branch for running: ${branch}"
-                }
+
             }
         }
 
